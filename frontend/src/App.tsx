db3fc6a31e5376df3edef887f944a984bbe79b7e
Ahmed Abd Ellaziz <99825342+1zizo1@users.noreply.github.com>
2024-09-28 16:43:07 +0300

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+}
+  from "react-router-dom";
+import Layout from "../layouts/Layout";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout></Layout>}>
+          <Route path="/Search" element={<>Search Page</>} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Route>
+      </Routes>
+
+    </Router>
+  );
+};
+export default App;
