97adcaf3212f9552358e87b2dbfb346419c68c7f
Ahmed Abd Ellaziz <99825342+1zizo1@users.noreply.github.com>
2024-11-20 11:52:38 +0200

    debug deploying error

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -12,14 +12,16 @@ declare global {
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies["auth_token"];
   if (!token) {
-    return res.status(401).json({ message: "unauthorized" ,token});
+    console.log("no token ");
+    
+    return res.status(401).json({ message: "unauthorized no token " ,token});
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
     req.userId = (decoded as JwtPayload).userId;
     next();
   } catch (error) {
-    return res.status(401).json({ message: "unauthorized" , error });
+    return res.status(401).json({ message: "unauthorized with error" , error });
   }
 };
 export default verifyToken;
