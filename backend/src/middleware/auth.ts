95ec96f6d99b8000fcef1440d28cc79c9c1ccbc3
Ahmed Abd Ellaziz <99825342+1zizo1@users.noreply.github.com>
2024-11-20 12:58:06 +0200

    debug the deployed error

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -15,7 +15,12 @@ const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     console.log("no token ");
     console.log("Cookies received:", req.cookies);
 
-    // return res.status(401).json({ message: "unauthorized no token " , req.cookies});
+    return res.status(401).json({ message: "unauthorized no token " , req});
+  }
+  // Ensure JWT_SECRET_KEY is loaded
+  if (!process.env.JWT_SECRET_KEY) {
+    console.error("JWT_SECRET_KEY is not defined");
+    return res.status(500).json({ message: "Internal server error: Missing JWT_SECRET_KEY" });
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
